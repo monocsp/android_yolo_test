e7744a76e5ccbaf546bfb09db8a4f37e1e956687
monocsp <monocsp@naver.com>
2024-03-04 10:33:39 +0900

    [update] android_camera test

---
+++ android_camera_test/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "cameratest_for_yolo"
+include(":app")
+ 
